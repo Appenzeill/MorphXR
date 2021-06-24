945943b745abef76b49445d1533ed80fefa99ccd
Appenzeill <daanroth@protonmail.com>
2021-06-24 17:13:53 +0200

    Documentatie geupdate

--- documentation/html/search/functions_2.js
+++ documentation/html/search/functions_2.js
@@ -1,5 +1,7 @@
 var searchData=
 [
+  ['printarm',['printArm',['../classoled__arm.html#a1703c08a80dd80a30fdaad12694af74f',1,'oled_arm']]],
   ['printarmaccel',['printArmAccel',['../classoled__arm.html#ae5eecd55d90a5c6137bfc4cef0cbcb2f',1,'oled_arm']]],
-  ['printarmgyro',['printArmGyro',['../classoled__arm.html#a35bcedc3e023c1744420fdc377ca980c',1,'oled_arm']]]
+  ['printarmgyro',['printArmGyro',['../classoled__arm.html#a35bcedc3e023c1744420fdc377ca980c',1,'oled_arm']]],
+  ['printarmlength',['printArmLength',['../classoled__arm.html#a4bb3a852d58e7febdf07d7342199813a',1,'oled_arm']]]
 ];
