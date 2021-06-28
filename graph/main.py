f18d0413c1c439ae5fc7d5738c32e1ffac61a1ab
Appenzeill <daanroth@protonmail.com>
2021-06-28 17:20:33 +0200

    Python script om serial af te vangen

--- graph/main.py
+++ graph/main.py
@@ -1,5 +1,51 @@
-import serial 
+import serial
 ser= serial.Serial('/dev/ttyACM0',2400, timeout=1)
+
+def rad():
+    return ser.read().decode()
+
+def read(arm):
+    tmp = ""
+    while(True):
+        read = rad()
+        if (read == "," or read == "*" or read == "a" or read == "b"):
+            break
+        else:
+            tmp += read
+    arm.append(int(tmp))
+    return arm
+        
+        
+bovenarm = []
+onderarm = []
+
 while(True):
-    read_ser=ser.read(size=15)
-    print(read_ser)
+    tmp = ""
+    bovenarm = []
+    onderarm = []
+
+    if (rad() == 'a'):
+        bovenarm = read(bovenarm)
+        bovenarm = read(bovenarm)
+        bovenarm = read(bovenarm)
+        bovenarm = read(bovenarm)
+        bovenarm = read(bovenarm)
+        bovenarm = read(bovenarm)
+        
+    if (rad() == 'b'):
+        onderarm = read(onderarm)
+        onderarm = read(onderarm)
+        onderarm = read(onderarm)
+        onderarm = read(onderarm)
+        onderarm = read(onderarm)
+        onderarm = read(onderarm)
+
+
+    if (len(bovenarm) != 0):
+        print("bovenarm")
+        print(bovenarm)
+    if (len(onderarm) != 0):
+        print("onderarm")
+        print(onderarm)
+
+                
