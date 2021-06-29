cbe1ceb85f49b05ec5d7c10d4da7f371d0bf447e
Appenzeill <daanroth@protonmail.com>
2021-06-29 20:50:07 +0200

    Threading toegevoegd bij Python script. Script leest en schrijft op 2 verschillende threads

--- graph/main.py
+++ graph/main.py
@@ -2,10 +2,13 @@ import serial
 import math
 import time
 import pickle
+import threading
+
+import numpy as np
 
 import matplotlib as mpl
 from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
+import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 
 
@@ -24,39 +27,48 @@ def read(arm):
     arm.append(int(tmp))
     return arm
         
+def write():
+    while(True):
+        tmp = ""
+        bovenarm = []
+        onderarm = []
 
-# clear file voor nieuwe data
-with open("arm.txt", 'r+') as f:
-    f.truncate(0)
-
-while(True):
-    tmp = ""
-    bovenarm = []
-    onderarm = []
-
-    if (rad() == 'a'):
-        bovenarm = read(bovenarm)
-        bovenarm = read(bovenarm)
-        bovenarm = read(bovenarm)
-        bovenarm = read(bovenarm)
-        bovenarm = read(bovenarm)
-        bovenarm = read(bovenarm)
+        if (rad() == 'a'):
+            bovenarm = read(bovenarm)
+            bovenarm = read(bovenarm)
+            bovenarm = read(bovenarm)
+            bovenarm = read(bovenarm)
+            bovenarm = read(bovenarm)
+            bovenarm = read(bovenarm)
         
-    if (rad() == 'b'):
-        onderarm = read(onderarm)
-        onderarm = read(onderarm)
-        onderarm = read(onderarm)
-        onderarm = read(onderarm)
-        onderarm = read(onderarm)
-        onderarm = read(onderarm)
+        if (rad() == 'b'):
+            onderarm = read(onderarm)
+            onderarm = read(onderarm)
+            onderarm = read(onderarm)
+            onderarm = read(onderarm)
+            onderarm = read(onderarm)
+            onderarm = read(onderarm)
 
-    if (len(onderarm) != 0 and len(bovenarm) != 0):
-        arm = [bovenarm,onderarm]
-        # angle = 180 - onderarm[3] - 90
-        # print(angle)
-        with open("arm.txt", "wb") as fp: 
-            pickle.dump(arm, fp)
+        if (len(onderarm) != 0 and len(bovenarm) != 0):
+            arm = [bovenarm,onderarm]
+            # angle = 180 - onderarm[3] - 90
+            # print(angle)
+            with open("arm.txt", "wb") as fp: 
+                pickle.dump(arm, fp)
 
-        
+def graph():
+    while(True):
+        time.sleep(0.01)
+        with open('arm.txt', 'rb') as file:
+            # Call load method to deserialze
+            myvar = pickle.load(file)
+        print(myvar)
 
-                
+def animate(i):
+    
+
+thread_a = threading.Thread(target=write)
+thread_b = threading.Thread(target=graph)
+
+thread_a.start()
+thread_b.start()
