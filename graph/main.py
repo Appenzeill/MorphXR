c37e667bbc1362c00ad2aae4d77546f83d576cf6
Appenzeill <daanroth@protonmail.com>
2021-06-30 11:30:57 +0200

    3D grafiek werkt nu op de verticale as

--- graph/main.py
+++ graph/main.py
@@ -11,7 +11,6 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 
-
 ser= serial.Serial('/dev/ttyACM0',2400, timeout=1)
 
 def rad():
@@ -51,24 +50,77 @@ def write():
 
         if (len(onderarm) != 0 and len(bovenarm) != 0):
             arm = [bovenarm,onderarm]
-            # angle = 180 - onderarm[3] - 90
-            # print(angle)
             with open("arm.txt", "wb") as fp: 
                 pickle.dump(arm, fp)
 
+def read_arm():
+    time.sleep(0.001)
+    with open('arm.txt', 'rb') as file:
+        read_arm = pickle.load(file)
+    return read_arm
+
+# matplotlib data
+fig = plt.figure()
+ax = plt.axes(projection ='3d')
+
+# waarden van axes
+ax.set_xlim(0, 100)
+ax.set_ylim(0, 100)
+ax.set_zlim(0, 100)
+
 def graph():
     while(True):
-        time.sleep(0.01)
-        with open('arm.txt', 'rb') as file:
-            # Call load method to deserialze
-            myvar = pickle.load(file)
-        print(myvar)
+        graph_data = read_arm()
+        print(graph_data)
+
 
 def animate(i):
+    graph_data = read_arm()
+    arm_length = 50
+
+    # onderarm
+    onderarm_angle = (180 - graph_data[1][3]) - 90 
+    onderarm_sine_degrees = math.sin(math.radians(onderarm_angle))
+    if (onderarm_sine_degrees != 0):
+        onderarm_divider = 1 / onderarm_sine_degrees
+    else:
+        onderarm_divider = 1 / 0.0001
+
+    onderarm_z = (arm_length / onderarm_divider)
+
+    onderarm_len_a = arm_length*arm_length
+    onderarm_len_b = onderarm_z*onderarm_z
+    onderarm_y = math.sqrt(onderarm_len_a - onderarm_len_b)
     
+    # bovenarm
+    bovenarm_angle = (180 - graph_data[0][3]) - 90 
+    bovenarm_sine_degrees = math.sin(math.radians(bovenarm_angle))
+    if (bovenarm_sine_degrees != 0):
+        bovenarm_divider = 1 / bovenarm_sine_degrees
+    else:
+        bovenarm_divider = 1 / 0.0001
+
+    bovenarm_z = (arm_length / bovenarm_divider)
+
+    bovenarm_len_a = arm_length*arm_length
+    bovenarm_len_b = bovenarm_z*bovenarm_z
+    bovenarm_y = math.sqrt(bovenarm_len_a - bovenarm_len_b)
+
+    bovenarm_z = bovenarm_z + 50
+    
+    X = [50,50,50]
+    Y = [0, bovenarm_y, bovenarm_y+onderarm_y]
+    Z = [50,bovenarm_z ,bovenarm_z+onderarm_z]
+    
+    ax.clear()
+    ax.set_xlim(0, 100)
+    ax.set_ylim(0, 100)
+    ax.set_zlim(0, 100)
+
+    ax.plot3D(X,Y,Z)
 
 thread_a = threading.Thread(target=write)
-thread_b = threading.Thread(target=graph)
-
 thread_a.start()
-thread_b.start()
+
+ani = animation.FuncAnimation(fig, animate, interval=100)
+plt.show()
